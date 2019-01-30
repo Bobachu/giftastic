7f153a8e100a2f39b42ded83f98a9abab03a0133
Chewie <43552746+Bobachu@users.noreply.github.com>
2019-01-29 19:02:54 -0700

    notes added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,38 +1,53 @@
-// pausing gifs activity
-// button trigger activity
-// an array of items
+// An array to store inital games and add new games to
 var videoGames = ["Final Fantasy", "Uncharted", "Destiny"];
-// adds a button to the screen for each item
+
+// A function to create new buttons, function declared
 function createButtons() {
+    // clears the area of buttons before creating new ones
     $("#game-buttons").empty();
+    // loops over the array...
     for (var i = 0; i < videoGames.length; i++) {
+        // to create a button for each item...
         var gameButton = $("<button>");
+        // and add the classes to use later (and also bootstrap)...
         gameButton.addClass("game btn btn-info");
+        // and create an attribute called data-name with a value of the game name, to be used later...
         gameButton.attr("data-name", videoGames[i]);
+        // and to add the game name as text on the buttons...
         gameButton.text(videoGames[i]);
+        // and to add the buttons to the appointed area on the HTML
         $("#game-buttons").append(gameButton);
     }
 };
 
+// A function to retrieve the gifs using the giphy api...
 function gameGifs() {
+    // variables for game name using the data-name attribute set above...
     var gameName = $(this).attr("data-name");
+    // and create a variable for the api url using the gameName variable
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         gameName + "&api_key=dc6zaTOxFJmzC&limit=10";
 
+    // An ajax call to the giphy api to request the gifs
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
+        // setting a variable to store the resonse array
         var results = response.data;
+        // console log the array (helps to get the needed paths)
         console.log(results);
+        // a for loop to loop through the created array...
         for (var i = 0; i < results.length; i++) {
+            // a variable to create a new div...
             var gifDiv = $("<div>");
-
+            // a div to store the gif rating
             var rating = results[i].rating;
-
+            // a variable to createa paragraph element and add the rating to it
             var p = $("<p>").text("Rating: " + rating);
+            // adding a rating-text class to the paragraphs
             p.addClass("rating-text");
-
+            
             var gameGif = $("<img>");
             gameGif.attr({
                 "src": results[i].images.original_still.url,
@@ -83,11 +98,3 @@ $(document).on("click", ".game", gameGifs);
 $(document).on("click", ".game-gifs", playPause);
 
 createButtons();
-// clicking the button contacts giphy to request related gifs (giphy api search)
-//images of related gifs are displayed
-// with their ratings
-// clicking on a gif causes it to animate
-// clicking again causes it to pause
-// take the value from a form
-// create a new button from it
-// new button performs the same as the old button
